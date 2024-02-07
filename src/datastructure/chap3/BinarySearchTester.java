216fa20847f0c69b6f63948c5d60d91e8e238dee
LAPTOP-OGNPS07V\moons <snowball9820@gmail.com>
2024-02-08 05:43:38 +0900

    datastructure/java.util.Arrays binarySearch

--- src/datastructure/chap3/BinarySearchTester.java
+++ src/datastructure/chap3/BinarySearchTester.java
@@ -28,7 +28,8 @@ public class BinarySearchTester {
         System.out.println("검색할 값: ");
         int ky = stdIn.nextInt();
 
-        int idx = Arrays.binarySearch(x, ky);//배열 x에서 값이 ky인 요소 검색 java.util.Arrays 클래스의 binarySearch->자바 표준 라이브러리
+        int idx = Arrays.binarySearch(x, ky);
+        //배열 x에서 값이 ky인 요소 검색 java.util.Arrays 클래스의 binarySearch->자바 표준 라이브러리
         //이진검색 메서드 직접 작성X, 배열요소 자료형 관계없이 검색 가능
 
         if (idx < 0) {
