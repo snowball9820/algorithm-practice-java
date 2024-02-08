dc0df80c3eea531bc1c8eb12e13072fc3f80985b
LAPTOP-OGNPS07V\moons <snowball9820@gmail.com>
2024-02-08 23:21:14 +0900

    datastructure/queue

---
+++ src/datastructure/chap4/IntArrayQueue.java
@@ -0,0 +1,99 @@
+package datastructure.chap4;
+
+public class IntArrayQueue {
+    private int[] que; //큐 본체
+    private int capacity; //큐 용량
+    private int num; //현재 데이터 개수
+
+    //실행 시 예외 : 큐가 비어 있음
+    public class EmptyIntArrayQueueException extends RuntimeException {
+        public EmptyIntArrayQueueException() { }
+    }
+
+    //실행 시 예외 : 큐가 가득 참
+    public class OverflowIntArrayQueueException extends RuntimeException {
+        public OverflowIntArrayQueueException() { }
+    }
+
+    //생성자
+    public IntArrayQueue(int maxlen) {
+        num=0;
+        capacity = maxlen;
+        try {
+            que = new int[capacity];
+        } catch (OutOfMemoryError error) {
+            capacity=0;
+        }
+    }
+
+    //인큐
+    public int enque(int x) throws OverflowIntArrayQueueException{
+        if (num >= capacity) {
+            throw new OverflowIntArrayQueueException();
+        }
+        que[num++]=x;
+        return x;
+    }
+    //디큐
+    public int deque()throws EmptyIntArrayQueueException {
+        if (num <= 0) {
+            throw new EmptyIntArrayQueueException();
+        }
+        int x = que[0]; //큐가 비어있음
+        for (int i = 0; i < num - 1; i++) {
+            que[i] = que[i + 1];
+        }
+        num--;
+        return x;
+    }
+    public int peek() throws EmptyIntArrayQueueException {
+        if (num <= 0)
+            throw new EmptyIntArrayQueueException();			// 큐가 비어 있음
+        return que[num - 1];
+    }
+
+    //큐에서 x를 검색하여 인덱스(발견하지 못하면 -1)를 반환
+    public int indexOf(int x) {
+        for (int i = 0; i < num; i++)
+            if (que[i]  == x)					// 검색 성공
+                return i;
+        return -1;									// 검색 실패
+    }
+
+    //큐를 비움
+    public void clear() {
+        num = 0;
+    }
+
+    // 큐의 용량을 반환
+    public int capacity() {
+        return capacity;
+    }
+
+    // 큐에 쌓여있는 데이터수를 반환
+    public int size() {
+        return num;
+    }
+
+    // 큐가 비어 있는가?
+    public boolean isEmpty() {
+        return num <= 0;
+    }
+
+    // 큐가 가득 찼는가?
+    public boolean isFull() {
+        return num >= capacity;
+    }
+
+    //큐 안의 모든 데이터를 맨앞 → 맨끝의 순서로 출력
+    public void dump() {
+        if (num <= 0)
+            System.out.println("큐가 비어 있습니다.");
+        else {
+            for (int i = 0; i < num; i++)
+                System.out.print(que[i] + " ");
+            System.out.println();
+        }
+    }
+
+}
