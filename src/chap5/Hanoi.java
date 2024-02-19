da7e6d2c4b860f7584c3bb8e1f5d709a559d0cb0
LAPTOP-OGNPS07V\moons <snowball9820@gmail.com>
2024-02-19 23:29:51 +0900

    algorithm/recursion_hanoi

--- src/chap5/Hanoi.java
+++ src/chap5/Hanoi.java
@@ -1,5 +1,7 @@
 package chap5;
 
+import java.util.Scanner;
+
 public class Hanoi {
     static void move(int no, int x, int y) {
         if (no > 1) {
@@ -10,6 +12,17 @@ public class Hanoi {
         if (no > 1) {
             move(no-1,6-x-y,y);
         }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("하노이의 탑 도전");
+        System.out.print("원반의 개수: ");
+        int n = scanner.nextInt();
+
+        move(n,1,3);//1번 기둥에 있는 n개의 원반을 3번 기둥으로 옮김
+
     }
 
 
